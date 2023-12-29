84510df76c24829cd8ed035381ed09d055634cf9
adelson70 <129080230+adelson70@users.noreply.github.com>
2023-12-29 07:22:31 -0300

    janela principal criada

--- gerador_senha.py
+++ gerador_senha.py
@@ -3,3 +3,18 @@ from random import choices
 from tkinter import messagebox
 import tkinter as tk
 from tkinter.font import Font
+
+
+
+
+
+
+
+
+
+# Janela principal da aplicação
+janela = tk.Tk()
+janela.title('Gerador de Senha Forte')
+
+
+janela.mainloop()
